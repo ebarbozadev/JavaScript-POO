3d3e6a5439ea6e8dc5b360e40abad9674edee0c7
ebarbozadev <e.barboza@edu.unipar.br>
2022-10-14 17:38:51 -0300

    Getter e Setter

--- User.js
+++ User.js
@@ -1,14 +1,51 @@
 // Sintaxe padrão da classe
 export default class User {
+    #nome
+    #email
+    #nascimento
+    #role
+    #ativo
     constructor(nome, email, nascimento, role, ativo = true){
-        this.nome = nome;
-        this.email = email;
-        this.role = role || "Estudante";
-        this.ativo = ativo;
+        this.#nome = nome;
+        this.#email = email;
+        this.#nascimento = nascimento;
+        this.#role = role || "Estudante";
+        this.#ativo = ativo;
     }
 
+    get nome (){
+        return this.#nome;
+    }
+
+    get email (){
+        return this.#email;
+    }
+
+    get nascimento (){
+        return this.#nascimento;
+    }
+
+    get role (){
+        return this.#role;
+    }
+
+    get ativo (){
+        return this.#ativo;
+    }
+
+    // #montaObjUser(){
+    //     return ({
+    //         nome: this.#nome,
+    //         email: this.#email,
+    //         nascimento: this.#nascimento,
+    //         role: this.#role,
+    //         ativo: this.#ativo
+    //     })
+    // }
+
     exibirInfos(){
-        return `${this.nome}, ${this.email}`;
+        const objUser = this.#montaObjUser();
+        return `${objUser.nome}, ${objUser.email}, ${objUser.nascimento}, ${objUser.role}, ${objUser.ativo}`;
     }
 }
 
--- index.js
+++ index.js
@@ -2,3 +2,15 @@ import User from './User.js';
 import Admin from './Admin.js';
 import Docente from './Docente.js';
 
+const newUser = new User('Emanuel', 'e@e.com', '18/04/2003');
+console.log(newUser.exibirInfos());
+
+const newAdmin = new Admin('Rafael', 'r@r.com', '31/05/1989');
+console.log(newAdmin.nome);
+
+
+// Privados, não podem ser mudados
+// newUser.#nome = 'Gabriel';
+// newUser.#email = 'g@g.com';
+// newUser.#nascimento = '20/08/1989';
+// console.log(newUser);
